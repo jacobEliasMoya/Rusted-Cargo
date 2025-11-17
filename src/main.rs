b3e91d2129c8fd817bab2095e8f4992c63d26095
jacob moya <jemoya1994@Gmail.com>
2025-11-16 23:41:24 -0800

    getting more into the Rust book, coding along with the guessing game, GPT on the side to further explain the literature

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
+use std::cmp::Ordering;
 use std::io;
 
+use rand::Rng;
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
 
-    let mut guess: String = String::new();
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    println!("The secret number is: {secret_number}");
 
-    println!("You guessed: {guess}");
+    loop {
+        println!("Please input your guess ");
+
+        let mut guess: String = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("To Small!"),
+            Ordering::Greater => println!("To Big!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
 }
- 
