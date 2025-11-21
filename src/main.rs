8c0ed6d90ea0abaf9aa919bab9c2b327e6f25666
jacob moya <jemoya1994@Gmail.com>
2025-11-20 23:32:10 -0800

    finished the lesson re 3.3, 3.4 is comments, no brainer - moving to 3.5

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,46 @@
+
 fn main() {
     println!("This is a function, some other function is below");
     some_other_function();
     function_with_parameters(79);
-    function_with_two_parameters(55,'J');
+    function_with_two_parameters(55, 'J');
+
+    let y: i32 = {
+        let x: i32 = 3;
+        x + 1
+    };  
+
+    println!("The value of y is: {y}");
+
+    let x: u32 = return_an_item();
+
+    println!("The returned value is: {x}");
+
+    let z:u32 = function_variable_retuned(1230);
+
+    println!("The returned value is: {z}");
+
 }
 
 // testing and adding in another function within main
 fn some_other_function() {
-    println!("This is that other function")
+    println!("This is that other function") // simple print to be used in main 
 }
 
 fn function_with_parameters(number: u32) {
-    println!("The number in this function is: {number}")
+    println!("The number in this function is: {number}") // returning the param
 }
 
 fn function_with_two_parameters(number: u32, unit_label: char) {
-    println!("The number in this function is: {number}:{unit_label}")
+    println!("The number in this function is: {number}:{unit_label}") // returning the 2 params
 }
+
+fn return_an_item() -> u32 {
+    5 // this is also returning something
+}
+
+fn function_variable_retuned(x:u32) -> u32 {
+    x + 1 // this is returning something
+}
+
+// comments, simple enough just a quick read and that should be good lol
