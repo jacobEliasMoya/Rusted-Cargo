9ad9fbe5d6c0b7529e2dcb34e1d2032d68d9e4fc
jacob moya <jemoya1994@Gmail.com>
2025-12-02 00:10:40 -0800

    Complete w/ 4.2 Moving to the slice type

--- src/main.rs
+++ src/main.rs
@@ -25,16 +25,23 @@ fn main() {
     let r2: &String = &mut s;
     println!("{r2}");
 
-    let mut mutable_string:String = String::from("value");
+    let mut mutable_string: String = String::from("value");
 
-    let immutable_one:&String = &mutable_string;
-    let immutable_two:&String = &mutable_string;
+    let immutable_one: &String = &mutable_string;
+    let immutable_two: &String = &mutable_string;
 
     println!("These are immutable {immutable_one} & {immutable_two}");
 
-    let mutable_three:&String = &mut mutable_string;
+    let mutable_three: &String = &mut mutable_string;
     println!("This is mutable {mutable_three}");
 
+    // ________________________________________________________//
+
+    // let reference_to_do_nothing: &String = dangler();
+
+    let no_dangle_ref: String = no_dangle();
+
+    println!("This is no_dangle_ref {no_dangle_ref}")
 }
 
 fn calculate_length(s: &String) -> usize {
@@ -43,4 +50,16 @@ fn calculate_length(s: &String) -> usize {
 
 fn change(some_string: &mut String) {
     some_string.push_str(", World");
+}
+
+// ententionally creating error
+// fn dangler() -> &String {
+//     let s: String = String::from("hello");
+//     &s
+// }
+
+fn no_dangle() -> String {
+    let s: String = String::from("Hello No Dangle");
+
+    s
 }
