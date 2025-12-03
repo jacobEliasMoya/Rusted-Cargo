0f1120d84047c1e9f0cfa6272851034be1b7e3fb
jacob moya <jemoya1994@Gmail.com>
2025-12-02 23:58:06 -0800

    reviewing iter and enumberate in detail

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 //The Slice Type
 fn main() {
+    let string: String = String::from("This is the value that I am looking at");
+    first_word(&string);
+}
 
+fn first_word(s: &String) -> usize {
+    let bytes: &[u8] = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }
