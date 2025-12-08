94903f413c0975bf47a469feec48ad43efe7519e
jacob moya <jemoya1994@Gmail.com>
2025-12-08 00:04:05 -0800

    added struct/ assigned values to user1/ printing lines to check using dot notation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
 fn main() {
     println!("Intro Into Structs");
+    println!("_________________________________________\n");
+
+    // defining user1 with the User struct
+    let user1: User = User {
+        active: true,
+        username: String::from("Jake"),
+        email: String::from("test@test.com"),
+        sign_in_count: 1,
+    };
+
+    // printing out and utilizing dot notation to select Srtuct fields
+    println!("This is our first user: {}", user1.username);
+    println!("_________________________________________\n");
+    println!("Is user1 {} is action: {}", user1.username, user1.active);
+    println!("_________________________________________\n");
+    println!("User1's email is as follows: {}", user1.email);
+    println!("_________________________________________\n");
+    println!("User1's has signed in: {} times", user1.sign_in_count);
 }
