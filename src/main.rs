34972345f58cb457bded7ce29023b6192fcac341
jacob moya <jemoya1994@Gmail.com>
2025-12-09 23:54:24 -0800

    refactor with tuples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    
+    // let width1: u32 = 30;
+    // let height1: u32 = 50;
+
+    // println!(
+    //     "The are of the rectangle is {} square pixles.",
+    //     area(width1, height1)
+    // );
+
+    let rect1: (u32, u32) = (30, 50);
+
+    println!("The are of the rectangle is {} square pixles.", area(rect1));
+}
+
+// v1
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
