dee665f726a123b9b59d77b1f3086d7ea10927ae
jacob moya <jemoya1994@Gmail.com>
2025-12-10 23:44:53 -0800

    wrapped up w/ 5.2 moving to 5.3 method syntax

---
+++ src/structs-program-example.rs
@@ -0,0 +1,46 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    // let width1: u32 = 30;
+    // let height1: u32 = 50;
+
+    // println!(
+    //     "The are of the rectangle is {} square pixles.",
+    //     area(width1, height1)
+    // );
+
+    // let rect1: (u32, u32) = (30, 50);
+    // println!("The are of the rectangle is {} square pixles.", area(rect1));
+
+    let scale: u32 = 2;
+    let rect1: Rectangle = Rectangle {
+        width: dbg!(scale * 30),
+        height: 50,
+    };
+
+    println!("rect1 = {rect1:#?}");
+
+    println!(
+        "The are of the rectangle is {} square pixles.",
+        area(&rect1)
+    );
+
+    dbg!(&rect1);
+}
+
+// v1
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.height * rectangle.width
+}
