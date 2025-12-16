9803320070cd6c5c9ec6a7dc666f1954765cc7a8
jacob moya <jemoya1994@Gmail.com>
2025-12-15 21:58:33 -0800

    Complete w/ ch5 > moving to ch6 enums

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,18 @@ impl Rectangle {
     fn just_height(&self) -> u32 {
         self.height
     }
+    fn width_bool(&self) -> bool {
+        self.width > 0
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -25,8 +37,24 @@ fn main() {
         height: 10,
     };
 
+    let rect2: Rectangle = Rectangle {
+        width: 100,
+        height: 5,
+    };
+
     println!("The perimeter of rect1 is {}", rect1.perimeter());
     println!("The area of rect1 is {}", rect1.area());
     println!("The width of rect1 is {}", rect1.just_width());
     println!("The height of rect1 is {}", rect1.just_height());
+
+    println!("The rect width is greater than 0: {}", rect1.width_bool());
+
+    println!(
+        "The rect1 width can contain rect2: {}",
+        rect1.can_hold(&rect2)
+    );
+
+    let r: Rectangle = Rectangle::square(55);
+    println!("The area of the new r is {}", r.area());
+    // println!("The square method on rectangle is: {:?}", r);
 }
