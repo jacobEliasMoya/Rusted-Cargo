e769fd103f76499763725f684f90c0ea101f5a9f
jacob moya <jemoya1994@Gmail.com>
2025-12-17 23:50:52 -0800

    Continuing pattern matching an solidifying enums

---
+++ src/defining-enums-p1.rs
@@ -0,0 +1,64 @@
+// enum IpAddrKind {
+//     V4(u8, u8, u8, u8),
+//     V6(String),
+// }
+
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+#[derive(Debug)]
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+// multiple embedded types within the enum below
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {}
+}
+
+fn main() {
+    println!("Ch.6: Enums");
+    // let four: IpAddr = IpAddr::V4(127,0,0,1);
+    // let six: IpAddr = IpAddr::V6(String::from("::1"));
+
+    let home: IpAddr = IpAddr::V4(127, 0, 0, 1);
+    let loopback: IpAddr = IpAddr::V6(String::from("::1"));
+
+    println!("This is the home IP: {:?}", home);
+    println!("This is the loopback IP: {:?}", loopback);
+
+    match home {
+        IpAddr::V4(a, b, c, d) => {
+            println!("IPv4 {}.{}.{}.{}", a, b, c, d);
+        }
+        IpAddr::V6(addr) => {
+            println!("IPv6 {}", addr);
+        }
+    }
+
+    let m: Message = Message::Write(String::from("Bananananan"));
+    m.call();
+
+    println!("asd");
+
+    // let home: IpAddr = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
+    // let loopback: IpAddr = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // };
+}
+
+// fn route(ip_kind: IpAddrKind) {}
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,22 @@
-// enum IpAddrKind {
-//     V4(u8, u8, u8, u8),
-//     V6(String),
-// }
+//
 
-// struct IpAddr {
-//     kind: IpAddrKind,
-//     address: String,
-// }
-
-#[derive(Debug)]
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
-}
-
-// multiple embedded types within the enum below
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
-
-impl Message {
-    fn call(&self) {
-    }
+enum Option<T> {
+    None,
+    Some(T),
 }
 
 fn main() {
-    println!("Ch.6: Enums");
+    let some_number: std::prelude::v1::Option<i32> = None;
+    let some_char: std::prelude::v1::Option<char> = Some('e');
 
-    // let four: IpAddr = IpAddr::V4(127,0,0,1);
-    // let six: IpAddr = IpAddr::V6(String::from("::1"));
+    let absent_number: std::prelude::v1::Option<i32> = None;
 
-    let home: IpAddr = IpAddr::V4(127, 0, 0, 1);
-    let loopback: IpAddr = IpAddr::V6(String::from("::1"));
+    println!("{:?}",some_number);
+    println!("{:?}",some_char);
+    println!("{:?}",absent_number);
 
-    println!("This is the home IP: {:?}", home);
-    println!("This is the loopback IP: {:?}", loopback);
-
-    match home {
-        IpAddr::V4(a, b, c, d) => {
-            println!("IPv4 {}.{}.{}.{}", a, b, c, d);
-        }
-        IpAddr::V6(addr) => {
-            println!("IPv6 {}", addr);
-        }
+    match some_number {
+        Some(n) => println!("{}",n),
+        None => println!("Some Number: No Value")
     }
-
-    let m: Message = Message::Write(String::from("Bananananan"));
-    m.call();
-
-    println!("{}" );
-
-    // let home: IpAddr = IpAddr {
-    //     kind: IpAddrKind::V4,
-    //     address: String::from("127.0.0.1"),
-    // };
-    // let loopback: IpAddr = IpAddr {
-    //     kind: IpAddrKind::V6,
-    //     address: String::from("::1"),
-    // };
 }
-
-// fn route(ip_kind: IpAddrKind) {}
