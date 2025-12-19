c7c716468473e23f4d24dba4c1b3be22e8bbb546
jacob moya <jemoya1994@Gmail.com>
2025-12-18 23:10:25 -0800

    adjustment to use borrowing and leave all ownership in main rather than pulling and moving ownership

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,17 @@ fn main() {
     let dime: Coin = Coin::Dime;
     let quarter: Coin = Coin::Quarter;
 
-    let penny_value: u8 = values_in_cents(penny);
-    let nickel_value: u8 = values_in_cents(nickel);
-    let dime_value: u8 = values_in_cents(dime);
-    let quarter_value: u8 = values_in_cents(quarter);
+    let penny_value: u8 = values_in_cents(&penny);
+    let nickel_value: u8 = values_in_cents(&nickel);
+    let dime_value: u8 = values_in_cents(&dime);
+    let quarter_value: u8 = values_in_cents(&quarter);
 
     println!(
         "Penny: {penny_value}, Nickel: {nickel_value}, Dime: {dime_value}, Quarter: {quarter_value}"
     );
 }
 
-fn values_in_cents(coin: Coin) -> u8 {
+fn values_in_cents(coin: &Coin) -> u8 {
     match coin {
         Coin::Penny => 1,
         Coin::Nickel => 5,
