e807b3fade7522d51aac1ff578dfd8918f1dbd74
jacob moya <jemoya1994@Gmail.com>
2025-12-24 00:39:38 -0800

    configuring new match method - using if let to be more concise

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 // concise control flow
 
-fn main() {}
+fn main() {
+    // saving numeric literal
+    let config_max: Option<u8> = Some(3u8);
+
+    // match config_max {
+    //     Some(max) => println!("Max configured to {max}"),
+    //     _ => (),
+    // }
+
+    if let Some(max) = config_max {
+        println!("Max configured to {max}")
+    } else {
+        println!("Not a max num")
+    }
+}
