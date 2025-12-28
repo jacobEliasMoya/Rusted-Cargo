2960070597071975575453ccf37b1176229ca8c6
jacob moya <jemoya1994@Gmail.com>
2025-12-28 00:27:44 -0800

    more work w/ old coin enum, using some snytactic sugar to just handle one case specifically, seems like a route for bad code, but should have minor use cases here and there

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 // concise control flow
+#[derive(Debug)]
 enum UsState {
     California,
 }
 enum Coin {
-    Penny,
-    Nickel,
     Dime,
     Quarter(UsState),
 }
@@ -12,16 +11,30 @@ fn main() {
     // saving numeric literal
     let config_max: Option<u8> = Some(3u8);
 
-    // match config_max {
-    //     Some(max) => println!("Max configured to {max}"),
-    //     _ => (),
-    // }
-    
+    match config_max {
+        Some(max) => println!("Max configured to {max}"),
+        _ => (),
+    };
+
     if let Some(max) = config_max {
         println!("Max configured to {max}")
     } else {
         println!("Not a max num")
     };
 
-    let mut count = 0;
+    let mut count: i32 = 0;
+    let coin1: Coin = Coin::Quarter(UsState::California);
+
+    // match coin1 {
+    //     Coin::Quarter(state) => println!("State Quarter from {state:?}"),
+    //     _ => count += 1,
+    // }
+
+    if let Coin::Quarter(state) = coin1 {
+        println!("State is {state:?}")
+    } else {
+        count += 1;
+    }
+
+    println!("{count}");
 }
