4c7c18d1016022e5c3e6dc4d6a9d080a18737a5c
jacob moya <jemoya1994@Gmail.com>
2025-12-29 21:36:50 -0800

    more consice control flow, need to work this out some more w/ an external project

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+use std::fmt::format;
+
 // concise control flow
 #[derive(Debug)]
 enum UsState {
     California,
 }
+
+impl UsState {
+    fn existed_in(&self, year: u16) -> bool {
+        match self {
+            UsState::California => year >= 1850,
+        }
+    }
+}
+
 enum Coin {
-    Dime,
     Quarter(UsState),
 }
 fn main() {
@@ -25,16 +35,27 @@ fn main() {
     let mut count: i32 = 0;
     let coin1: Coin = Coin::Quarter(UsState::California);
 
-    // match coin1 {
-    //     Coin::Quarter(state) => println!("State Quarter from {state:?}"),
-    //     _ => count += 1,
-    // }
-
-    if let Coin::Quarter(state) = coin1 {
-        println!("State is {state:?}")
-    } else {
-        count += 1;
+    match coin1 {
+        Coin::Quarter(state) => println!("State Quarter from {state:?}"),
     }
 
+    // if let Coin::Quarter(state) = coin1 {
+    //     println!("State is {state:?}")
+    // } else {
+    //     count += 1;
+    // }
+
     println!("{count}");
+}
+
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+    if let Coin::Quarter(state) = coin {
+        if state.existed_in(1900) {
+            Some(format!("{state:?} is pretty old, for America!"))
+        } else {
+            Some(format!("{state:?} is relatively new."))
+        }
+    } else {
+        None
+    }
 }
