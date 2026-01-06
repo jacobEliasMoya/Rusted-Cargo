504f739c916d7b5cd169ce8cab03d8e541f129a7
jacob moya <jemoya1994@Gmail.com>
2026-01-06 00:04:30 -0800

    Working more closely w/ vectors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
 // Common collections
 fn main() {
-    
+    let mut v:Vec<i32> = Vec::new();
+    // let vec1: Vec<i32> = vec![1,2,3];
+
+    v.push(1);
+    v.push(2);
+    v.push(3);
+
+    let third: &i32 = &v[3];
+
+    println!("{v:?}");
 }
