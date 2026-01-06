8e5454b715ed3a3faed4a95d3fbb5092e9e571ec
jacob moya <jemoya1994@Gmail.com>
2026-01-05 23:29:57 -0800

    ch8 - common collections

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,68 @@
-use std::{fmt::Result,io::Result as IoResult};
+// use std::{fmt::Result,io::Result as IoResult};
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
-    }
+//         fn seat_at_table() {}
+//     }
 
-    pub use crate::front_of_house::hosting as foh_hosting;
+//     pub use crate::front_of_house::hosting as foh_hosting;
 
-    mod serving {
-        fn take_order() {}
+//     mod serving {
+//         fn take_order() {}
 
-        fn serve_order() {}
+//         fn serve_order() {}
 
-        fn take_payment() {}
-    }
-}
-
-fn deliver_order() {}
-
-mod back_of_house {
-
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-
-    pub struct Breakfast {
-        pub toast: String,
-        pub seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
-    }
-
-    fn cook_order() {}
-}
-
-mod customer {
-    use crate::front_of_house::hosting::add_to_waitlist;
-
-    pub fn eat_at_restaurant() {
-        // let order1 = back_of_house::Appetizer::Soup;
-        // let order2 = back_of_house::Appetizer::Salad;
-        add_to_waitlist();
-    }
-}
-
-// fn function1() -> Result {
-
+//         fn take_payment() {}
+//     }
 // }
 
-// fn function()-> IoResult<()>{
+// fn deliver_order() {}
 
+// mod back_of_house {
+
+//     pub enum Appetizer {
+//         Soup,
+//         Salad,
+//     }
+
+//     pub struct Breakfast {
+//         pub toast: String,
+//         pub seasonal_fruit: String,
+//     }
+
+//     impl Breakfast {
+//         pub fn summer(toast: &str) -> Breakfast {
+//             Breakfast {
+//                 toast: String::from(toast),
+//                 seasonal_fruit: String::from("peaches"),
+//             }
+//         }
+//     }
+
+//     fn fix_incorrect_order() {
+//         cook_order();
+//         super::deliver_order();
+//     }
+
+//     fn cook_order() {}
 // }
+
+// mod customer {
+//     use crate::front_of_house::hosting::add_to_waitlist;
+
+//     pub fn eat_at_restaurant() {
+//         // let order1 = back_of_house::Appetizer::Soup;
+//         // let order2 = back_of_house::Appetizer::Salad;
+//         add_to_waitlist();
+//     }
+// }
+
+// // fn function1() -> Result {
+
+// // }
+
+// // fn function()-> IoResult<()>{
+
+// // }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-use crate::garden::plant;
-use crate::garden::vegetable::name_the_veggie;
-// Packages, crates, and modules
-// pt.1 packages and crates
-pub mod garden;
+// Common collections
 fn main() {
-    // println!("Hello this is a crate");
-    plant();
-    name_the_veggie();
+    
 }
