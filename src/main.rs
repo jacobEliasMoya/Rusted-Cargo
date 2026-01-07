350233baa9fcdd93f2dcc2cd16f5d5458c65f80a
jacob moya <jemoya1994@Gmail.com>
2026-01-06 23:30:52 -0800

    the get method on this is much better using the option type, much safer and more explicit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 // Common collections
 fn main() {
-    let mut v:Vec<i32> = Vec::new();
+    let mut v: Vec<i32> = Vec::new();
     // let vec1: Vec<i32> = vec![1,2,3];
 
     v.push(1);
     v.push(2);
-    v.push(3);
+    v.push(2);
+    v.push(2);
+    v.push(2);
 
-    let third: &i32 = &v[3];
+    // let third: &i32 = &v[2];  will intentionally cause panic -
 
-    println!("{v:?}");
+    let third: Option<&i32> = v.get(2);
+
+    match third {
+        Some(third) => print!("Good to go, valid index here: {third}"),
+        None => println!("Error, no valid index"),
+    }
+
+    // println!("In vector:{v:?} '{third:?}' is the 3rd item");
 }
