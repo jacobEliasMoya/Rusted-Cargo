c9e8cd5e0b70816ef759b980557bddf30a030bd5
jacob moya <jemoya1994@Gmail.com>
2026-01-11 23:18:33 -0800

    logging before EOD, more Rust before my next ambitious project lol

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 // More into the UTF-8 String
 
 fn main() {
-    let mut s: String = String::new();
-    let data: String = "initial contents".to_string();
+    let s = String::from("initial contents");
+    // let mut s: String = String::new();
+    // let data: String = "initial contents".to_string();
     // let s: String = data.to_string();
-    println!("{data}");
+    println!("{s}");
+
+    // updating a string
+
+    let mut str: String = String::from("foo");
+    str.push_str("bar");
+
+    println!("{str}");
+
+    let mut s1:String = String::from("foo");
+    let s2 = "bar";
+
+    s1.push_str(s2);
+
+    println!("{s1}");
+
+
 }
