365e35445804b913561245aa88680959c03c0fcd
jacob moya <jemoya1994@Gmail.com>
2026-01-13 23:57:07 -0800

    dead tired, resuming more tomorrow

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,12 @@
-// hash maps
-fn main() {}
+use std::collections::HashMap;
+
+fn main() {
+    println!("badum hashmap");
+
+    let mut scores: HashMap<String, u32> = HashMap::new();
+
+    scores.insert(String::from("Blue"), 58);
+    scores.insert(String::from("Yellow"), 158);
+
+    println!("{scores:?}");
+}
