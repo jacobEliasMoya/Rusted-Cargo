f943308da9d16f86a7cab266c0e70966bc07ed04
jacob moya <jemoya1994@Gmail.com>
2026-01-14 23:31:22 -0800

    More playing around with Rusts hash maps

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,25 @@ fn main() {
     scores.insert(String::from("Yellow"), 158);
 
     println!("{scores:?}");
+
+    let team_name: String = String::from("Blue");
+    let score: u32 = scores.get(&team_name).copied().unwrap_or(0);
+
+    println!("This is the score of {team_name}: {score}");
+
+    for (k, v) in scores {
+        println!("This is the score of {k}: {v}");
+    }
+
+    let field_name: String = String::from("Favortie color");
+    let field_value: String = String::from("Royal Blue");
+
+    let mut new_map: HashMap<String, String> = HashMap::new();
+
+    new_map.insert(field_name, field_value);
+
+    for (k, v) in new_map {
+        println!("My {k} is {v}");
+    }
+
 }
