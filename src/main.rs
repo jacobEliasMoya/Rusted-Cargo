ecf6f59800cdc3ee1385a3cb8b3c83dbd767b4dc
jacob moya <jemoya1994@Gmail.com>
2026-01-14 23:56:08 -0800

    Tinkering and tinkering, the only way to learn XD

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,22 @@ fn main() {
         println!("This is the score of {k}: {v}");
     }
 
-    let field_name: String = String::from("Favortie color");
+    let field_name: String = String::from("Favorite color");
     let field_value: String = String::from("Royal Blue");
 
     let mut new_map: HashMap<String, String> = HashMap::new();
 
     new_map.insert(field_name, field_value);
 
-    for (k, v) in new_map {
+    for (k, v) in &new_map {
         println!("My {k} is {v}");
     }
 
+    let updated_field: String = String::from("Hot Pink");
+
+    new_map.insert(String::from("Favorite color"), updated_field);
+
+    for (k, v) in &new_map {
+        println!("My {k} is {v}");
+    }
 }
