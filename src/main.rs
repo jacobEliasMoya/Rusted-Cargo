835a0c550128eec12862f914e4d9b1fde8c1ef77
jacob moya <jemoya1994@Gmail.com>
2026-01-20 00:29:52 -0800

    more tinkering with Result enum and additional files/ present or not

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 // Error Handling in Rust
+use std::fs::File;
 
 fn main() {
-    let v: Vec<i32> = vec![1, 2, 3];
+    // let v: Vec<i32> = vec![1, 2, 3];
 
-    for i in &v {
-        println!("{i}");
-    }
+    // for i in &v {
+    //     println!("{i}");
+    // }
 
-    v[99];
+    let greeting_file_result: Result<File, std::io::Error> = File::open("hello.txt");
+
+    let greeting_file: File = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {error:?}"),
+    };
+
+    println!("{greeting_file:?}");
 }
