89c73825780d94268f34e044f00e8b80ec5dfd1c
jacob moya <jemoya1994@Gmail.com>
2026-02-03 00:15:44 -0800

    Generic func implementation: Returning highest generic val from a slice

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,11 @@
 
 fn main() {
     let number_list = vec![34, 50, 36, 100, 65];
-    let result = largest_i32(&number_list);
+    let result = largest_generic(&number_list);
     println!("This is the larget number: {result}");
 
     let char_list = vec!['y', 'm', 'a', 'q'];
-    let result = largest_char(&char_list);
+    let result = largest_generic(&char_list);
     println!("This is the larget number: {result}");
 }
 
@@ -35,6 +35,16 @@ fn largest_i32(list: &[i32]) -> &i32 {
 fn largest_char(list: &[char]) -> &char {
     let mut largest = &list[0];
 
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn largest_generic<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
     for item in list {
         if item > largest {
             largest = item;
