2b78d0da911e9138c5b6d066c425dac8a31fb8e3
Jake <jemoya1994@gmail.com>
2026-02-02 22:03:53 -0800

    More rust, and trying utilize nvim better lol ... god yes I love pain

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,44 @@
-fn main() {}
+// Generic Data Types
+
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for number in list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 36, 100, 65];
+    let result = largest(&number_list);
+    println!("This is the larget number: {result}");
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = largest(&number_list);
+    println!("This is the larget number: {result}");
+}
+
+// fn largest_i32() -> &i32 {
+//     let mut largest = &list[0];
+//
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+//
+// fn largest_char(list: &[char]) -> &char {
+//     let mut largest = &list[0];
+//
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
