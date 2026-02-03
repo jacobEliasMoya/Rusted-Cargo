08e73c45a270037a33c00ba9db8a520e9a4166a3
jacob moya <jemoya1994@Gmail.com>
2026-02-02 23:43:23 -0800

    More work w/ generics, now on 10.1

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,44 @@
 // Generic Data Types
 
-fn largest(list: &[i32]) -> &i32 {
+// fn largest(list: &[i32]) -> &i32 {
+//     let mut largest = &list[0];
+//
+//     for number in list {
+//         if number > largest {
+//             largest = number;
+//         }
+//     }
+//     largest
+// }
+
+fn main() {
+    let number_list = vec![34, 50, 36, 100, 65];
+    let result = largest_i32(&number_list);
+    println!("This is the larget number: {result}");
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest_char(&char_list);
+    println!("This is the larget number: {result}");
+}
+
+fn largest_i32(list: &[i32]) -> &i32 {
     let mut largest = &list[0];
 
-    for number in list {
-        if number > largest {
-            largest = number;
+    for item in list {
+        if item > largest {
+            largest = item;
         }
     }
     largest
 }
 
-fn main() {
-    let number_list = vec![34, 50, 36, 100, 65];
-    let result = largest(&number_list);
-    println!("This is the larget number: {result}");
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
-    println!("This is the larget number: {result}");
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
 }
-
-// fn largest_i32() -> &i32 {
-//     let mut largest = &list[0];
-//
-//     for item in list {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
-//     largest
-// }
-//
-// fn largest_char(list: &[char]) -> &char {
-//     let mut largest = &list[0];
-//
-//     for item in list {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
-//     largest
-// }
