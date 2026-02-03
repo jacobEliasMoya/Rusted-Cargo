ec5e4211049a921dbab5eb0add31648bb005c7b0
jacob moya <jemoya1994@Gmail.com>
2026-02-03 00:20:07 -0800

    Minor spelling fix

--- src/main.rs
+++ src/main.rs
@@ -14,34 +14,34 @@
 fn main() {
     let number_list = vec![34, 50, 36, 100, 65];
     let result = largest_generic(&number_list);
-    println!("This is the larget number: {result}");
+    println!("This is the largest number: {result}");
 
     let char_list = vec!['y', 'm', 'a', 'q'];
     let result = largest_generic(&char_list);
-    println!("This is the larget number: {result}");
+    println!("This is the largest char: {result}");
 }
 
-fn largest_i32(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
-
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
-}
-
-fn largest_char(list: &[char]) -> &char {
-    let mut largest = &list[0];
-
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
-}
+// fn largest_i32(list: &[i32]) -> &i32 {
+//     let mut largest = &list[0];
+//
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+//
+// fn largest_char(list: &[char]) -> &char {
+//     let mut largest = &list[0];
+//
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
 
 fn largest_generic<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
