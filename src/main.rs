d245f7434fda5952ae673693ddc19f1e82999f40
jacob moya <jemoya1994@Gmail.com>
2026-02-04 23:24:42 -0800

    More generics, and impl on generic types, iotuib aksi  exists to specify specific implementations on non generics with generic existing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 // Generic Data Types
 #[derive(Debug)]
-struct Point<T, U> {
+struct Point<T> {
     x: T,
-    y: U,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn y(&self) -> &T {
+        &self.y
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
 }
 
 // fn largest(list: &[i32]) -> &i32 {
@@ -15,23 +31,33 @@ struct Point<T, U> {
 //     }
 //     largest
 // }
+//
 
 fn main() {
     let number_list = vec![34, 50, 36, 100, 65];
     let result = largest_generic(&number_list);
     println!("This is the largest number: {result}");
 
-    let char_list = vec!['y', 'm', 'a', 'q', 'r'];
-    let result = largest_generic(&char_list);
-    println!("This is the largest char: {result}");
+    let p = Point { x: 6, y: 7 };
 
-    let integer = Point { x: 5, y: 10 };
-    let float = Point { x: 5.0, y: 10.0 };
-    let float_int = Point { x: 20, y: 10.0 };
+    println!("p.x = {}", p.x());
+    println!("p.y = {}", p.y());
 
-    println!("{integer:?}");
-    println!("{float:?}");
-    println!("{float_int:?}");
+    let p_float = Point { x: 1.5, y: 2.4 };
+
+    println!("distance: {}", p_float.distance_from_origin());
+
+    // let char_list = vec!['y', 'm', 'a', 'q', 'r'];
+    // let result = largest_generic(&char_list);
+    // println!("This is the largest char: {result}");
+    //
+    // let integer = Point { x: 5, y: 10 };
+    // let float = Point { x: 5.0, y: 10.0 };
+    // let float_int = Point { x: 20, y: 10.0 };
+    //
+    // println!("{integer:?}");
+    // println!("{float:?}");
+    // println!("{float_int:?}");
 }
 // fn largest_i32(list: &[i32]) -> &i32 {
 //     let mut largest = &list[0];
