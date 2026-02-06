92ec6cdc35a26bd96a7827f82922af6f836c6534
jacob moya <jemoya1994@Gmail.com>
2026-02-06 00:11:05 -0800

    Working post summarizer

---
+++ src/generic-data-types.rs
@@ -0,0 +1,114 @@
+// Generic Data Types
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+#[derive(Debug)]
+struct MixPoint<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> MixPoint<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: MixPoint<X2, Y2>) -> MixPoint<X1, Y2> {
+        MixPoint {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn y(&self) -> &T {
+        &self.y
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+// fn largest(list: &[i32]) -> &i32 {
+//     let mut largest = &list[0];
+//
+//     for number in list {
+//         if number > largest {
+//             largest = number;
+//         }
+//     }
+//     largest
+// }
+//
+
+fn main() {
+    // let number_list = vec![34, 50, 36, 100, 65];
+    //
+    // println!("This is the largest number: {result}");
+    //
+    // let p = Point { x: 6, y: 7 };
+    //
+    // println!("p.x = {}", p.x());
+    // println!("p.y = {}", p.y());
+    //
+    let p_float = Point { x: 1.5, y: 2.4 };
+
+    println!("distance: {}", p_float.distance_from_origin());
+
+    let p1 = MixPoint { x: 5, y: 10.4 };
+    let p2 = MixPoint { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {} p3.y = {}", p3.x, p3.y);
+
+    // let char_list = vec!['y', 'm', 'a', 'q', 'r'];
+    // let result = largest_generic(&char_list);
+    // println!("This is the largest char: {result}");
+    //
+    // let integer = Point { x: 5, y: 10 };
+    // let float = Point { x: 5.0, y: 10.0 };
+    // let float_int = Point { x: 20, y: 10.0 };
+    //
+    // println!("{integer:?}");
+    // println!("{float:?}");
+    // println!("{float_int:?}");
+}
+// fn largest_i32(list: &[i32]) -> &i32 {
+//     let mut largest = &list[0];
+//
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+//
+// fn largest_char(list: &[char]) -> &char {
+//     let mut largest = &list[0];
+//
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+
+fn largest_generic<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,40 @@
-// Generic Data Types
-#[derive(Debug)]
-struct Point<T> {
-    x: T,
-    y: T,
+// Defining Shared Behavior with Traits
+pub trait Summary {
+    fn summarize(&self) -> String;
 }
 
-#[derive(Debug)]
-struct MixPoint<X1, Y1> {
-    x: X1,
-    y: Y1,
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
 }
 
-impl<X1, Y1> MixPoint<X1, Y1> {
-    fn mixup<X2, Y2>(self, other: MixPoint<X2, Y2>) -> MixPoint<X1, Y2> {
-        MixPoint {
-            x: self.x,
-            y: other.y,
-        }
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
     }
 }
 
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
-    }
-
-    fn y(&self) -> &T {
-        &self.y
-    }
+pub struct SocialPost {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub repost: bool,
 }
 
-impl Point<f32> {
-    fn distance_from_origin(&self) -> f32 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
+impl Summary for SocialPost {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
     }
 }
-
-// fn largest(list: &[i32]) -> &i32 {
-//     let mut largest = &list[0];
-//
-//     for number in list {
-//         if number > largest {
-//             largest = number;
-//         }
-//     }
-//     largest
-// }
-//
 
 fn main() {
-    // let number_list = vec![34, 50, 36, 100, 65];
-    //
-    // println!("This is the largest number: {result}");
-    //
-    // let p = Point { x: 6, y: 7 };
-    //
-    // println!("p.x = {}", p.x());
-    // println!("p.y = {}", p.y());
-    //
-    let p_float = Point { x: 1.5, y: 2.4 };
-
-    println!("distance: {}", p_float.distance_from_origin());
-
-    let p1 = MixPoint { x: 5, y: 10.4 };
-    let p2 = MixPoint { x: "Hello", y: 'c' };
-
-    let p3 = p1.mixup(p2);
-
-    println!("p3.x = {} p3.y = {}", p3.x, p3.y);
-
-    // let char_list = vec!['y', 'm', 'a', 'q', 'r'];
-    // let result = largest_generic(&char_list);
-    // println!("This is the largest char: {result}");
-    //
-    // let integer = Point { x: 5, y: 10 };
-    // let float = Point { x: 5.0, y: 10.0 };
-    // let float_int = Point { x: 20, y: 10.0 };
-    //
-    // println!("{integer:?}");
-    // println!("{float:?}");
-    // println!("{float_int:?}");
-}
-// fn largest_i32(list: &[i32]) -> &i32 {
-//     let mut largest = &list[0];
-//
-//     for item in list {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
-//     largest
-// }
-//
-// fn largest_char(list: &[char]) -> &char {
-//     let mut largest = &list[0];
-//
-//     for item in list {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
-//     largest
-// }
-
-fn largest_generic<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
-    let mut largest = &list[0];
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
+    let post = NewsArticle {
+        headline: String::from("Bananas Poison"),
+        location: String::from("California"),
+        author: String::from("Ron Burgundy"),
+        content: String::from("This just in, bananas are poison"),
+    };
+    println!("1 NewsArticle: {}", post.summarize());
 }
