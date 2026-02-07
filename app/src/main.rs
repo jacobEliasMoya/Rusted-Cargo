2a533e7a397defb6f7f0df749df64f9ff1ff83f7
Jake <jemoya1994@gmail.com>
2026-02-07 00:58:44 -0800

    Minor updates to the app, testing traits & developing a better understanding

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,12 +2,21 @@
 use aggregator::{NewsArticle, SocialPost, Summary};
 
 fn main() {
-    let post = NewsArticle {
+    let article = NewsArticle {
         headline: String::from("Bananas Poison"),
         location: String::from("California"),
         author: String::from("Ron Burgundy"),
         content: String::from("This just in, bananas are poison"),
     };
 
-    println!("1 NewsArticle: {}", post.summarize());
+    println!("1 NewsArticle: {}", article.summarize());
+
+    let post = SocialPost {
+        username: String::from("JoeShmo"),
+        content: String::from("There is is a banana"),
+        reply: false,
+        repost: false,
+    };
+
+    println!("New Post: {}", post.summarize());
 }
