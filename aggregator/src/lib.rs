3668a63a9a3eb29dbe9d2fa95bf556b045438a8e
jacob moya <jemoya1994@Gmail.com>
2026-02-08 22:51:42 -0800

    updating more traits - and generics

--- aggregator/src/lib.rs
+++ aggregator/src/lib.rs
@@ -2,6 +2,10 @@ pub trait Summary {
     fn summarize(&self) -> String;
 }
 
+pub trait Loggable {
+    fn log_content(&self) -> String;
+}
+
 pub struct NewsArticle {
     pub headline: String,
     pub location: String,
@@ -12,6 +16,12 @@ pub struct NewsArticle {
 impl Summary for NewsArticle {
     fn summarize(&self) -> String {
         format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+impl Loggable for NewsArticle {
+    fn log_content(&self) -> String {
+        format!("{}", self.content)
     }
 }
 
@@ -30,4 +40,12 @@ impl Summary for SocialPost {
 
 pub fn notify(item: &impl Summary) {
     println!("Breaking new! {}", item.summarize());
+}
+
+pub fn notify_both<T: Summary>(item: &T) {
+    println!("Breaking new! {}", item.summarize());
+}
+
+pub fn log_line(item: &impl Loggable) {
+    println!("Line logged: {}", item.log_content());
 }
